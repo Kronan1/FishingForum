777b336e640be1a12e83536dde54fc5552204c22
Kronan1 <110824570+Kronan1@users.noreply.github.com>
2024-05-21 11:39:06 +0200

    Updated SubCategory and Thread

--- FishingForum/Models/SubCategory.cs
+++ FishingForum/Models/SubCategory.cs
@@ -5,11 +5,11 @@ namespace FishingForum.Models
     public class SubCategory
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
 
         [ForeignKey("Category")]
         public int CategoryId { get; set; }
-        public virtual Category Category { get; set; }
+        public virtual Category Category { get; set; } = new();
     }
 }
--- FishingForum/Models/Thread.cs
+++ FishingForum/Models/Thread.cs
@@ -10,12 +10,12 @@ namespace FishingForum.Models
 
         [ForeignKey("SubCategory")]
         public int SubCategoryId { get; set; }
-        public virtual SubCategory SubCategory { get; set; }
+        public virtual SubCategory SubCategory { get; set; } = new();
 
 
         [ForeignKey("FishingForumUser")]
         public int UserId { get; set; }
-        public virtual Areas.Identity.Data.FishingForumUser CreatedBy { get; set; }
+        public virtual Areas.Identity.Data.FishingForumUser CreatedBy { get; set; } = new();
 
         public DateTime DateCreated { get; set; }
     }
